e84221de94baf81afa112b0cb4f2b552ff5b7560
Hack Reactor Students <klp2012@gmail.com>
2014-08-07 12:26:59 -0700

    queueViews + queueEntryView properly renders and updates

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,19 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
   },
 
   render: function() {
-    return this.$el;
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
